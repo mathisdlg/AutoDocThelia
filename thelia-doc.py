22e90497d34a0531918704df7f79a6179e9a8603
mathisdlg <delage.mathis.1@gmail.com>
2024-05-02 09:45:44 +0200

    Add loops documentation generation feature and upgrade the code by removing duplication

--- thelia-doc.py
+++ thelia-doc.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-import sys
+import sys, os
 import Modules.hooks as hooks, Modules.events as events, Modules.commands as commands, Modules.check as check
+import Modules.loopAnalyser as loops
 
 
 def help():
@@ -10,6 +11,7 @@ def help():
     print("[2] - go to hooks documentation manager")
     print("[3] - generate events documentation")
     print("[4] - generate commands documentation")
+    print("[5] - generate loops documentation")
     print("[9] - generate all documentation")
     print("")
     print("[0] - exit: Exit the program\n")
@@ -39,29 +41,16 @@ def interacive():
                 directory = input("Enter the path to the Thelia directory: ")
                 toFile = input("Enter the documentation path to modify: ")
                 commands.main(directory, toFile)
+            case '5':
+                loopFolder = input("Enter the path to the loop folder: ")
+                loopDoc = input("Enter the path to the loop documentation file: ")
+                loops.main(loopFolder, loopDoc)
             case '9':
                 rootThelia = input("Enter the path to the root of the Thelia project to scan (Thelia Core): ")
                 rootDoc = input("Enter the root path of the documentation to modify: ")
                 orderHook = input("Would you like to sort the hooks: ").lower()
 
-                if orderHook != "n":
-                    orderHook = True
-
-                if not rootThelia.endswith("/"):
-                    rootThelia += "/"
-                if not rootDoc.endswith("/"):
-                    rootDoc += "/"
-
-                hooksDoc = rootDoc + "docs/hooks.md"
-                eventsDoc = rootDoc + "docs/events.md"
-                commandsDoc = rootDoc + "docs/commands/"
-
-                theliaEvents = rootThelia + "core/lib/Thelia/Core/Event/"
-
-                events.main(theliaEvents, eventsDoc)
-                hooks.all(rootThelia, hooksDoc, orderHook)
-                hooks.clean()
-                commands.main(rootThelia, commandsDoc)
+                generate(rootThelia, rootDoc, orderHook)
             case _:
                 print("Invalid choice. Please try again.\n")
                 continue
@@ -70,29 +59,20 @@ def interacive():
     sys.exit(0)
 
 
-def generate():
-    rootThelia = sys.argv[3]
-    rootDoc = sys.argv[4]
-    if len(sys.argv) > 5:
-        orderHook = sys.argv[5].lower()
-    else:
-        orderHook = "y"
+def generate(rootThelia, rootDoc, orderHook="y"):
+    hooksDoc = os.path.join(rootDoc, "docs/hooks.md")
+    eventsDoc = os.path.join(rootDoc, "docs/events.md")
+    commandsDoc = os.path.join(rootDoc, "docs/commands/")
+    loopDoc = os.path.join(rootDoc, "docs/loops/")
 
-    if not rootThelia.endswith("/"):
-        rootThelia += "/"
-    if not rootDoc.endswith("/"):
-        rootDoc += "/"
-
-    hooksDoc = rootDoc + "docs/hooks.md"
-    eventsDoc = rootDoc + "docs/events.md"
-    commandsDoc = rootDoc + "docs/commands/"
-
-    theliaEvents = rootThelia + "core/lib/Thelia/Core/Event/"
+    theliaEvents = os.path.join(rootThelia, "core/lib/Thelia/Core/Event/")
+    theliaLoops = os.path.join(rootThelia, "core/lib/Thelia/Core/Template/Loop/")
 
     events.main(theliaEvents, eventsDoc)
     hooks.all(rootThelia, hooksDoc, orderHook)
     hooks.clean()
     commands.main(rootThelia, commandsDoc)
+    loops.main(theliaLoops, loopDoc)
 
 
 def check():
@@ -104,7 +84,13 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         if sys.argv[1] == "--CI":
             if len(sys.argv)>4 and sys.argv[2] == "--generate":
-                generate()
+                rootThelia = sys.argv[3]
+                rootDoc = sys.argv[4]
+                if len(sys.argv) > 5:
+                    orderHook = sys.argv[5].lower()
+                else:
+                    orderHook = "y"
+                generate(rootThelia, rootDoc, orderHook)
                 sys.exit(0)
             elif len(sys.argv)>3 and sys.argv[2] == "--check":
                 check()
